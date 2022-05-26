13fd94b0890229cc5a345c2b74fdb387959c522d
jonatasfilipe <jonatasalvarenga@outlook.com>
2022-05-26 08:27:51 -0300

    Adicionar arquivos de projeto.

---
+++ AppBuscaCEP/PessoaJuridica.cs
@@ -0,0 +1,143 @@
+﻿namespace RequisicoesWeb
+{ 
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class AtividadePrincipal
+    {
+        public string id { get; set; }
+        public string secao { get; set; }
+        public string divisao { get; set; }
+        public string grupo { get; set; }
+        public string classe { get; set; }
+        public string subclasse { get; set; }
+        public string descricao { get; set; }
+    }
+
+    public class AtividadesSecundaria
+    {
+        public string id { get; set; }
+        public string secao { get; set; }
+        public string divisao { get; set; }
+        public string grupo { get; set; }
+        public string classe { get; set; }
+        public string subclasse { get; set; }
+        public string descricao { get; set; }
+    }
+
+    public class Cidade
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int ibge_id { get; set; }
+        public string siafi_id { get; set; }
+    }
+
+    public class Estabelecimento
+    {
+        public string cnpj { get; set; }
+        public List<AtividadesSecundaria> atividades_secundarias { get; set; }
+        public string cnpj_raiz { get; set; }
+        public string cnpj_ordem { get; set; }
+        public string cnpj_digito_verificador { get; set; }
+        public string tipo { get; set; }
+        public string nome_fantasia { get; set; }
+        public string situacao_cadastral { get; set; }
+        public string data_situacao_cadastral { get; set; }
+        public string data_inicio_atividade { get; set; }
+        public object nome_cidade_exterior { get; set; }
+        public string tipo_logradouro { get; set; }
+        public string logradouro { get; set; }
+        public string numero { get; set; }
+        public object complemento { get; set; }
+        public string bairro { get; set; }
+        public string cep { get; set; }
+        public string ddd1 { get; set; }
+        public string telefone1 { get; set; }
+        public object ddd2 { get; set; }
+        public object telefone2 { get; set; }
+        public object ddd_fax { get; set; }
+        public object fax { get; set; }
+        public object email { get; set; }
+        public object situacao_especial { get; set; }
+        public object data_situacao_especial { get; set; }
+        public DateTime atualizado_em { get; set; }
+        public AtividadePrincipal atividade_principal { get; set; }
+        public Pais pais { get; set; }
+        public Estado estado { get; set; }
+        public Cidade cidade { get; set; }
+        public object motivo_situacao_cadastral { get; set; }
+        public List<object> inscricoes_estaduais { get; set; }
+    }
+
+    public class Estado
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string sigla { get; set; }
+        public int ibge_id { get; set; }
+    }
+
+    public class NaturezaJuridica
+    {
+        public string id { get; set; }
+        public string descricao { get; set; }
+    }
+
+    public class Pais
+    {
+        public string id { get; set; }
+        public string iso2 { get; set; }
+        public string iso3 { get; set; }
+        public string nome { get; set; }
+        public string comex_id { get; set; }
+    }
+
+    public class Porte
+    {
+        public string id { get; set; }
+        public string descricao { get; set; }
+    }
+
+    public class QualificacaoDoResponsavel
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+    }
+
+    public class QualificacaoSocio
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+    }
+
+    public class PessoaJuridica
+    {
+        public string cnpj_raiz { get; set; }
+        public string razao_social { get; set; }
+        public string capital_social { get; set; }
+        public string responsavel_federativo { get; set; }
+        public DateTime atualizado_em { get; set; }
+        public Porte porte { get; set; }
+        public NaturezaJuridica natureza_juridica { get; set; }
+        public QualificacaoDoResponsavel qualificacao_do_responsavel { get; set; }
+        public List<Socio> socios { get; set; }
+        public object simples { get; set; }
+        public Estabelecimento estabelecimento { get; set; }
+    }
+
+    public class Socio
+    {
+        public string cpf_cnpj_socio { get; set; }
+        public string nome { get; set; }
+        public string tipo { get; set; }
+        public string data_entrada { get; set; }
+        public string cpf_representante_legal { get; set; }
+        public object nome_representante { get; set; }
+        public string faixa_etaria { get; set; }
+        public DateTime atualizado_em { get; set; }
+        public string pais_id { get; set; }
+        public QualificacaoSocio qualificacao_socio { get; set; }
+        public object qualificacao_representante { get; set; }
+    }
+
+
+}
---
+++ AppBuscaCEP/Program.cs
@@ -0,0 +1,206 @@
+﻿using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using AppBuscaCEP;
+using RequisicoesWeb;
+
+namespace RequisicoesWeb
+{
+    public class WebRequestGetExample
+    {
+        class Sample
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("   ------BUSCADOR------   ");
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("");
+                inicio();
+
+            }
+
+            private static void inicio()
+            {
+                Console.WriteLine("1 - Pesquisar CEP");
+                Console.WriteLine("2 - Pesquisar CNPJ");
+                Console.WriteLine("3 - Sair");
+                var opcao = Console.ReadLine();
+
+
+                switch (opcao)
+                {
+                    case "1":
+                        PesquisaCEP();
+                        break;
+                     case "2":
+                         PesquisaCNPJ();
+                         break;
+                    case "3":
+                        Sair();
+                        break;
+                    default:
+                        //Console.Clear();
+                        Console.WriteLine("Opção inválida");
+                        inicio();
+                        break;
+                }
+            }
+            static void PesquisaCEP()
+            {
+
+                Console.WriteLine("Digite o CEP: (apenas números)");
+                string codigoPostal = "";
+
+                codigoPostal = Console.ReadLine();
+                codigoPostal = codigoPostal.Replace(".", "").Replace("/", "").Replace("-", "").Replace(" ", "").Replace(",", "");
+                Console.WriteLine("Buscando dados...");
+                Console.WriteLine("");
+
+                if(codigoPostal.Length == 8)
+                {
+                    Endereco endereco = BuscarCEP(codigoPostal);
+                    Console.WriteLine($"{" \n Seu endereço é: "}{"\n "}{endereco.logradouro}{"; \n Bairro: "}{endereco.bairro}{"; \n Cidade: "}{endereco.localidade}{"; \n Estado: "}{endereco.uf}{";"}");
+                    Console.WriteLine("--------------------");
+                    Console.WriteLine("--------------------");
+                    inicio();
+                }
+                else
+                {
+                    Console.WriteLine("CEP inválido");
+                    Console.WriteLine("1 - Tentar novamente");
+                    Console.WriteLine("2 - Voltar ao menu");
+                    Console.WriteLine("3 - Sair");
+                    var opcao_2 = Console.ReadLine();
+                    Console.WriteLine("-------------------------");
+                    switch (opcao_2)
+                    {
+                        case "1":
+                            PesquisaCEP();
+                            break;
+                        case "2":
+                            inicio();
+                            break;
+                        case "3":
+                            Sair();
+                            break;
+                        default :
+                            Console.WriteLine("Opção inválida");
+                            inicio();
+                            break;
+                    }
+                }
+                
+
+            }
+
+            static void PesquisaCNPJ()
+            {
+                Console.WriteLine("Digite o CNPJ: (apenas números)");
+                string cnpj = "";
+                cnpj = Console.ReadLine();
+                cnpj = cnpj.Replace("/", "").Replace(".", "").Replace("-", "").Replace(" ", "").Replace(",", "");
+
+                Console.WriteLine("Buscando dados...");
+                Console.WriteLine("");
+                if(cnpj.Length == 14)
+                {
+                    PessoaJuridica pessoaJuridica = BuscarCNPJ(cnpj);
+                    Console.WriteLine($"{" \n CNPJ raiz: "}{pessoaJuridica.cnpj_raiz}{"; \n Razão social: "}{pessoaJuridica.razao_social}{"; \n Capital Social: "}{pessoaJuridica.capital_social}");
+                    Console.WriteLine("--------------------");
+                    Console.WriteLine("--------------------");
+                    inicio();
+                }
+                else
+                {
+                    Console.WriteLine("CEP inválido");
+                    Console.WriteLine("1 - Tentar novamente");
+                    Console.WriteLine("2 - Voltar ao menu");
+                    Console.WriteLine("3 - Sair");
+                    var opcao_3 = Console.ReadLine();
+                    Console.WriteLine("-------------------------");
+                    switch (opcao_3)
+                    {
+                        case "1":
+                            PesquisaCNPJ();
+                            break;
+                        case "2":
+                            inicio();
+                            break;
+                        case "3":
+                            Sair();
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida");
+                            inicio();
+                            break;
+                    }
+                }
+
+            }
+            static void Sair()
+            {
+                Console.WriteLine("Saindo...");
+            }
+
+
+        }
+
+        //aplica uma requisição na URL
+        static Endereco BuscarCEP(string codigoPostal)
+        {
+
+            var requisicaoWeb = WebRequest.CreateHttp($"https://viacep.com.br/ws/{codigoPostal}/json/");
+
+            requisicaoWeb.Method = "GET";
+            requisicaoWeb.UserAgent = "RequisicaoWebDemo";
+
+            //guarda os dados da web em variavel
+            using (var resposta = requisicaoWeb.GetResponse())
+            {
+                var streamDados = resposta.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                object objResponse = reader.ReadToEnd();
+                string retorno = objResponse.ToString();
+;
+                streamDados.Close();
+                resposta.Close();
+
+
+
+                Endereco endereco =
+                JsonConvert.DeserializeObject<Endereco>(retorno);
+                return endereco;
+            }
+
+        }
+
+        //aplica uma requisição na URL
+        static PessoaJuridica BuscarCNPJ(string cnpj)
+        {
+
+            var requisicaoWeb = WebRequest.CreateHttp($"https://publica.cnpj.ws/cnpj/{cnpj}");
+
+            requisicaoWeb.Method = "GET";
+            requisicaoWeb.UserAgent = "RequisicaoWebDemo";
+
+            //guarda os dados da web em variavel
+            using (var resposta = requisicaoWeb.GetResponse())
+            {
+                var streamDados = resposta.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                object objResponse = reader.ReadToEnd();
+                string retornar = objResponse.ToString();
+
+                streamDados.Close();
+                resposta.Close();
+
+
+                PessoaJuridica pessoaJuridica =
+                JsonConvert.DeserializeObject<PessoaJuridica>(retornar);
+                return pessoaJuridica;
+            }
+
+        }
+    }
+}
